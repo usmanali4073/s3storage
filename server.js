8d45d9a2cf998ef12d7c7970024ce552a385dabf
usmanali4073 <36858540+usmanali4073@users.noreply.github.com>
2020-02-10 11:24:41 -0500

    Updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const homeRoute = require('./routes/homeRoute');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.use(homeRoute)
 
 
 
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port port!`+port))
